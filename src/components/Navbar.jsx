096e267866a70164b301c1d6c701c87f67c78bd7
Kazuchan1889 <134587893+Kazuchan1889@users.noreply.github.com>
2025-08-04 15:51:38 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,9 +26,6 @@ const Navbar = () => {
             ))}
           </ul>
           <div className="hidden lg:flex justify-center space-x-12 items-center">
-            <a href="#" className="py-2 px-3 border rounded-md">
-              Sign In
-            </a>
             <a
               href="#"
               className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
@@ -52,14 +49,11 @@ const Navbar = () => {
               ))}
             </ul>
             <div className="flex space-x-6">
-              <a href="#" className="py-2 px-3 border rounded-md">
-                Sign In
-              </a>
               <a
                 href="#"
                 className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
               >
-                Create an account
+                Contact Us
               </a>
             </div>
           </div>
