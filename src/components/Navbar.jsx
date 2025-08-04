8ae9b6e89a96117bce0ae80d421c90f62e49b16c
gelaptaknampak <133974027+gelaptaknampak@users.noreply.github.com>
2025-08-04 16:38:16 +0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,9 +14,7 @@ const Navbar = () => {
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
-          <div className="flex items-center flex-shrink-0">
-            <img className="h-auto w-36 mr-2" src={logo} alt="Logo" />
-          </div>
+          <img className="h-36 w-36 mr-2" src={logo} alt="Logo" />
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
               <li key={index}>
@@ -29,7 +27,7 @@ const Navbar = () => {
               href="#"
               className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
             >
-              Create an account
+              Contact Us
             </a>
           </div>
           <div className="lg:hidden md:flex flex-col justify-end">
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -16,6 +16,7 @@ export const navItems = [
   { label: "About Us", href: "#" },
   { label: "Services", href: "#" },
   { label: "portofolio", href: "#" },
+  { label: "Blog", href: "#" },
   // { label: "Testimonials", href: "#" },
 ];
 
