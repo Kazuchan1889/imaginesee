14d9d30a295bd35f5381f202296c1ac33082b256
Kazuchan1889 <134587893+Kazuchan1889@users.noreply.github.com>
2025-08-07 09:51:12 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,27 +15,41 @@ const Navbar = () => {
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
           <img className="w-36 mr-2" src={logo} alt="Logo" />
-          <ul className="hidden lg:flex ml-14 space-x-12">
+
+          {/* Desktop Navigation */}
+          <ul className="hidden lg:flex  space-x-12 items-center">
             {navItems.map((item, index) => (
-              <li key={index}>
-                <a href={item.href}>{item.label}</a>
+              <li key={index} className="relative group flex items-center justify-center">
+                <a
+                  href={item.href}
+                  className="uppercase text-[16px] text-[#e1e1e1] transition-colors duration-400 ease-[cubic-bezier(0.25,0.8,0.25,1)] group-hover:text-white focus:text-white block text-center"
+                >
+                  {item.label}
+                  <span className="absolute inset-x-0 mx-auto -bottom-[2px] h-[2px] w-0 bg-white transition-all duration-400 ease-[cubic-bezier(0.25,0.8,0.25,1)] group-hover:w-full group-focus:w-full"></span>
+                </a>
               </li>
             ))}
           </ul>
+
+          {/* Contact Button (Desktop) */}
           <div className="hidden lg:flex justify-center space-x-12 items-center">
             <a
               href="#"
-              className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+              className="bg-gradient-to-r from-[#58A0C8] to-[#34699A] py-2 px-3 rounded-md"
             >
               Contact Us
             </a>
           </div>
+
+          {/* Mobile Menu Button */}
           <div className="lg:hidden md:flex flex-col justify-end">
             <button onClick={toggleNavbar}>
               {mobileDrawerOpen ? <X /> : <Menu />}
             </button>
           </div>
         </div>
+
+        {/* Mobile Navigation */}
         {mobileDrawerOpen && (
           <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
             <ul>
@@ -48,7 +62,7 @@ const Navbar = () => {
             <div className="flex space-x-6">
               <a
                 href="#"
-                className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+                className="py-2 px-3 rounded-md bg-gradient-to-r from-[#58A0C8] to-[#34699A]"
               >
                 Contact Us
               </a>
